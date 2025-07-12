56d8bc08b93ee5abb972a7185bc75fddec6f2574
1kamil0 <kamilaakk07@gmail.com>
2025-07-12 10:01:30 -0700

    Add files via upload

---
+++ student grading.py
@@ -0,0 +1,17 @@
+name =str(input("enter the name"))
+number = int(input("enter the number"))
+if number>=91 and number<=100:
+    print("kamil your grad is A+")
+if number>=81 and number<=90:
+    print("kamil your grade is A")
+if number>=65 and number<=80:
+    print("kamil your grade is B+")
+if number>=45 and number<=64:
+    print("kamil your grade is B")
+if number>=35 and number<=44:
+    print("kamil your grade is C+")
+if number>=18 and number<=34:
+    print("kamil your grade is C")
+if number>=1 and number<=17:
+    print("kamil your grade is D")
+    
